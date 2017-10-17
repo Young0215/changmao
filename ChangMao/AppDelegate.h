6f9291681e8f86511fac944099da24f9e88dd54b
景睦科技 <jingmukeji@jingmukejideMacBook-Pro.local>
2017-10-17 15:51:43 +0800

    Initial Commit

---
+++ ChangMao/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChangMao
+//
+//  Created by 景睦科技 on 2017/10/17.
+//  Copyright © 2017年 景睦科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
